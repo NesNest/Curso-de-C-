7097a8bc24588a2fff71a54b92b0e79c6916e30e
Néstor Guerrero <72157425+PlasticTramp@users.noreply.github.com>
2023-12-05 19:51:47 -0600

    Agrege todo cs

---
+++ Cs/formatocadena/Program.cs
@@ -0,0 +1,5 @@
+﻿string second = "world";
+string first = "Hello";
+
+string result = string.Format("{0} {1}! {0}", first, second);
+Console.WriteLine(result);
---
+++ Cs/test2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+int saludM = 10;
+int saludH = 10;
+Random rd = new Random();
+
+do{
+    int hit = rd.Next(1,11);
+    saludM -= hit;
+    
+    if(saludM < 0){
+        
+        Console.WriteLine("golpe es de " + hit + " El moustro esta muerto");
+    } 
+    else{
+        Console.WriteLine("Al monstro le quitan " + hit + " y se queda con " + saludM);
+        hit = rd.Next(1,11);
+        saludH -= hit;
+
+        if(saludH < 0){
+            Console.WriteLine("golpe de " + hit + " El heroe esta muerto");
+        }
+        else{
+            Console.WriteLine("Al Heroe le quitan " + hit + " y se queda con " + saludH);
+
+        }
+    }
+
+}while(saludM > 0);
